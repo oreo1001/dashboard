7fca70bfa675f294b773530f4c0220759330fa68
oreo1001 <ohsimon77@naver.com>
2024-02-23 19:44:59 +0900

    completed screen

--- src/app/completed/completedContent.tsx
+++ src/app/completed/completedContent.tsx
@@ -30,9 +30,47 @@ export default function CompletedContent() {
           size={100}
         ></IoCheckmark>
         <div className="mt-14 text-xl md:text-3xl font-semibold text-center px-3">
-          Your Donna E-mail has been issued!
+          You are ready to use Donna!
         </div>
+
+        <div className="mt-10 text-md md:text-lg xl:text-xl font-semibold text-center px-3">
+          Donna needs to learn a bit more about you to become personalized for
+          you!
+        </div>
+
+        <div className="mt-2 text-md md:text-lg xl:text-xl font-semibold text-center px-3">
+          Below is the email I sent to suggest an onboarding meeting so that it
+          can be more tailored to you.
+        </div>
+
+        <div className="mt-2 text-md md:text-lg xl:text-xl font-semibold text-center px-3">
+          Try asking Donna to schedule a meeting with me!
+        </div>
+
         <div className="mt-8 flex flex-row border-[2px] border-stone-200 rounded-xl shadow-sm">
+          <div className="relative flex justify-center items-center my-auto rounded-full ml-5">
+            <Image
+              className="rounded-full"
+              src="/assets/김정원.jpeg"
+              alt="cover"
+              width={70}
+              height={70}
+              placeholder="blur"
+              blurDataURL="/assets/placeholder.png"
+            />
+          </div>
+          <div className="py-3 flex flex-col ml-4 mr-5">
+            <div className="font-semibold text-xl">
+              Jungwon(jungwon@wonmo.net)
+            </div>
+            <div className="text-md">Nice to e-meet you!</div>
+            <div className="text-stone-400">
+              Hello I&apos;m Jungwon, I want to know your preferences to
+              schedule ...
+            </div>
+          </div>
+        </div>
+        {/* <div className="mt-8 flex flex-row border-[2px] border-stone-200 rounded-xl shadow-sm">
           <div className="flex">
             <div className="relative flex justify-center items-center my-auto w-[70px] h-[70px] rounded-full ml-5">
               <Image
@@ -46,7 +84,7 @@ export default function CompletedContent() {
             </div>
             <div className="py-3 flex flex-col ml-4 mr-5">
               <div className="font-semibold text-xl">
-                Donna(Donna@wonmo.net!)
+                Donna(donna@wonmo.net)
               </div>
               <div className="text-md">Nice to e-meet you!</div>
               <div className="text-stone-400">
@@ -54,7 +92,7 @@ export default function CompletedContent() {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
         <div className="flex flex-row">
           <div className="pt-8 text-xl md:text-2xl text-stone-500">
             Check your&nbsp;
